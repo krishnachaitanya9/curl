6cdd8b7498d15df1da283f25fc45ced3b46d38f9
Krishna <chaitanyaradon89@gmail.com>
2020-07-21 08:36:27 -0600

    Make data save logged

--- train.py
+++ train.py
@@ -178,7 +178,7 @@ def main():
     env_name = args.domain_name + '-' + args.task_name
     exp_name = env_name + '-' + ts + '-im' + str(args.image_size) + '-b' \
                + str(args.batch_size) + '-s' + str(args.seed) + '-' + args.encoder_type
-    args.work_dir = args.work_dir + '\\' + exp_name
+    args.work_dir = args.work_dir + '/' + exp_name
 
     utils.make_dir(args.work_dir)
     video_dir = utils.make_dir(os.path.join(args.work_dir, 'video'))
