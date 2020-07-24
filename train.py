8ea16662169595d70cc41b57de60f96c1f49906f
Krishna <chaitanyaradon89@gmail.com>
2020-07-24 05:30:15 -0600

    Some Slight Modifications

--- train.py
+++ train.py
@@ -60,7 +60,7 @@ def parse_args():
     # misc
     parser.add_argument('--seed', default=1, type=int)
     parser.add_argument('--work_dir', default='.', type=str)
-    parser.add_argument('--save_tb', default=False, action='store_true')
+    parser.add_argument('--save_tb', default=True, action='store_true')
     parser.add_argument('--save_buffer', default=False, action='store_true')
     parser.add_argument('--save_video', default=True, action='store_true')
     parser.add_argument('--save_model', default=True, action='store_true')
